1c62e4a8e08a8b3ec2ea42542f742067bb9f0a46
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-08 17:32:10 +0100

    Simplified activation

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,7 @@
 # Run the pyessv test suite.
 #
 
-HERMES_CLIENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $HERMES_CLIENT_DIR
-export PYTHONPATH=PYTHONPATH:$HERMES_CLIENT_DIR
+HERMES_CLIENT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $HERMES_CLIENT_HOME
+export PYTHONPATH=PYTHONPATH:$HERMES_CLIENT_HOME
 nosetests -v -s tests
